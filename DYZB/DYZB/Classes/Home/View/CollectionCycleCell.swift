4d954d08e352c8a9c23e8970c9da7beca22e63cc
CGLueng <842341047@qq.com>
2016-12-17 16:05:43 +0800

    首页图片无限轮播功能

--- DYZB/DYZB/Classes/Home/View/CollectionCycleCell.swift
+++ DYZB/DYZB/Classes/Home/View/CollectionCycleCell.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class CollectionCycleCell: UICollectionViewCell {
     
+    // MARK:- 控件属性
+    @IBOutlet weak var icomImageView: UIImageView!
+    @IBOutlet weak var titleLB: UILabel!
+    
+    
     // MARK:- 定义属性
     var cycleModel : CycleModel? {
         didSet {
-            
+            titleLB.text = cycleModel?.titile
+            let iconURL = URL(string: cycleModel?.pic_url ?? "")!
+            icomImageView.kf.setImage(with: iconURL, placeholder: UIImage(named: "Img_default"))
         }
     }
     
--- DYZB/DYZB/Classes/Home/View/CommendCycleView.swift
+++ DYZB/DYZB/Classes/Home/View/CommendCycleView.swift
@@ -13,12 +13,19 @@ private let kCycleCellID = "kCycleCellID"
 class CommendCycleView: UIView {
     
     // MARK:- 定义属性
+    var cycleTimer : Timer?
     var cycleModelArr : [CycleModel]? {
         didSet {
             //刷新 collectionview
             collectionView.reloadData()
             //设置 pagecontrol 个数
             pageControl.numberOfPages = cycleModelArr?.count ?? 0
+            //默认滚动到中间某一个位置
+            let indexPath = NSIndexPath(item: (cycleModelArr?.count ?? 0) * 100, section: 0)
+            collectionView.scrollToItem(at: indexPath as IndexPath, at: .left, animated: false)
+            //添加定时器
+            removeTimer()
+            addCycleTimer()
         }
     }
     
@@ -61,16 +68,59 @@ extension CommendCycleView {
 // MARK:- 遵守 UIcollectionview 数据源
 extension CommendCycleView : UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cycleModelArr?.count ?? 0
+        return (cycleModelArr?.count ?? 0) * 10000
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         //拿出数据
-        let cycleModel = cycleModelArr![indexPath.item]
+        let cycleModel = cycleModelArr![indexPath.item % cycleModelArr!.count]
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kCycleCellID, for: indexPath) as! CollectionCycleCell
         cell.cycleModel = cycleModel
         
         return cell
+    }
+}
+
+// MARK:- 遵守 UIcollectionview 代理
+extension CommendCycleView : UICollectionViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //获取滚动偏移量
+        let offsetX = scrollView.contentOffset.x + scrollView.bounds.width * 0.5
+        //计算 pagecontrol的 currentIndex
+        pageControl.currentPage = Int(offsetX / scrollView.bounds.width) % (cycleModelArr?.count ?? 1)
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        //用户滑动时候移除定时器
+        removeTimer()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        //用户停止滑动时候添加定时器
+        addCycleTimer()
+    }
+}
+
+// MARK:- 对定时器的操作方法
+extension CommendCycleView {
+    fileprivate func addCycleTimer() {
+        cycleTimer = Timer(timeInterval: 2.0, target: self, selector: #selector(scrollToNext), userInfo: nil, repeats: true)
+        //添加到运行循环中
+        RunLoop.main.add(cycleTimer!, forMode: .commonModes)
+    }
+    
+    fileprivate func removeTimer() {
+        //从运行循环中移除
+        cycleTimer?.invalidate()
+        cycleTimer = nil
+    }
+    
+    @objc fileprivate func scrollToNext() {
+        //计算偏移量
+        let currentOffsetX = collectionView.contentOffset.x
+        let offsetX = currentOffsetX + collectionView.bounds.width
+        //滚动到该位置
+        collectionView.setContentOffset(CGPoint(x: offsetX, y: 0), animated: true)
     }
 }
